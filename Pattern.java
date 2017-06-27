65dc337eeea662855e2f295e1bd25f10b2d53c57
aditigoyal04 <aditigoyal41996@gmail.com>
2017-06-28 00:48:13 +0530

    Add files via upload

--- Pattern.java
+++ Pattern.java
@@ -1,15 +1,10 @@
-class Rtriangle 
+public class Pattern 
 {
 public static void main(String[] args) 
 {
-
 	for(int i=0; i<4; i++)
-	{	
-		for(int k=0; k<=i; k++)
-		{
-		System.out.print(' ');
-		}
-		for(int j=0; j<7-(2*i); j++)
+	{
+		for(int j=0; j<=(2*i); j++)
 		{
 		System.out.print("*");
 		}
@@ -19,10 +14,11 @@ public static void main(String[] args)
 }
 
 /*
+ 
+*
+***
+*****
 *******
- *****
-  ***
-   *
 
 
 */
--- Triangle.java
+++ Triangle.java
@@ -3,9 +3,9 @@ class Triangle
 public static void main(String[] args) 
 {
 
-	for(int i=0; i<3; i++)
+	for(int i=0; i<4; i++)
 	{	
-		for(int k=0; k<=2-i; k++)
+		for(int k=0; k<=3-i; k++)
 		{
 		System.out.print(' ');
 		}
@@ -17,3 +17,11 @@ public static void main(String[] args)
 	}
 }
 }
+
+/*
+    *
+   ***
+  *****
+ *******
+
+*/
